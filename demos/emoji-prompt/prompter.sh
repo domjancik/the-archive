7d13f2772d1664dcb7c694bf4485b07a64090e23
Dominik Jancik <hi@domj.net>
2022-04-01 18:29:04 +0300

    demos/emoji-prompt use correct prompt linex

--- demos/emoji-prompt/prompter.sh
+++ demos/emoji-prompt/prompter.sh
@@ -2,9 +2,9 @@
 
 function PROMPTER() {
     # CUTPWD=$(python3 -c "L=10;d='`pwd`';print(('...' if len(d) > L else '')+'`pwd`'[-L:])")
-    # export PS1=`get_emoji.py`" "$(echo -e "\e[1;30m`hyper_pwd.py`\e[0m ")
+    export PS1=`get_emoji.py`" "$(echo -e "\e[1;30m`hyper_pwd.py`\e[0m ")
     # export PS1=`get_emoji.py`" " 
-    PS1=$RANDOM
+    # PS1=$RANDOM
 }
 
 export PROMPT_COMMAND=PROMPTER
