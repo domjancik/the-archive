331f619c03853fdda6f204f6938a55cee3d7f94c
Dominik Jancik <hi@domj.net>
2022-04-02 17:28:27 +0300

    emoji-prompt updates

--- demos/emoji-prompt/prompter.sh
+++ demos/emoji-prompt/prompter.sh
@@ -2,7 +2,8 @@
 
 function PROMPTER() {
     # CUTPWD=$(python3 -c "L=10;d='`pwd`';print(('...' if len(d) > L else '')+'`pwd`'[-L:])")
-    export PS1=`get_emoji.py`" "$(echo -e "\e[1;30m`hyper_pwd.py`\e[0m ")
+    # export PS1=`get_emoji.py`" "$(echo -e "\e[1;30m`hyper_pwd.py`\e[0m ")
+    export PS1=`get_emoji.py`" "$(hyper_pwd.py)" "
     # export PS1=`get_emoji.py`" " 
     # PS1=$RANDOM
 }
